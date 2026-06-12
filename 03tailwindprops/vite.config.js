697d62fabc69eae41ac272c24f40daac6280a2db
Priyanka Pandit <priyankapandit0012gmail.com>
2026-06-12 16:22:02 +0530

    second

--- 03tailwindprops/vite.config.js
+++ 03tailwindprops/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+})
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// Export Default Defineconfig Plugins React  
